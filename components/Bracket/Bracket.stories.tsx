dfb1499dedfe144139f0f6a6f47b3492aeda7e79
Emmanuel Pilande <epilande@gmail.com>
2019-03-27 22:59:28 -0700

    :books: Add Round story

--- components/Bracket/Bracket.stories.tsx
+++ components/Bracket/Bracket.stories.tsx
@@ -4,9 +4,19 @@ import { text, boolean } from "@storybook/addon-knobs";
 import Bracket from "./Bracket";
 import Contender from "./Contender";
 import Match from "./Match";
+import Round from "./Round";
 
 storiesOf("Bracket", module)
   .add("Default", () => <Bracket>Hello</Bracket>)
+  .add("Round", () => (
+    <Round
+      matches={[
+        ["react", "vue"],
+        ["angular", "mithril"],
+        // ["preact", "knockout"],
+      ]}
+    />
+  ))
   .add("Match", () => (
     <svg width="200" height="300">
       <Match
