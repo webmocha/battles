7c735ed5160ce2ea2fb3dbd8361c07ca5f77f25c
Emmanuel Pilande <epilande@gmail.com>
2019-03-27 22:18:36 -0700

    :pushpin: Make Match component have dynamic size and positioning

--- components/Bracket/Match.tsx
+++ components/Bracket/Match.tsx
@@ -1,23 +1,40 @@
 import * as React from "react";
+import useBounds, { Bounds } from "../hooks/useBounds";
 import Contender, { Props as ContenderProps } from "./Contender";
 
 interface Props {
   contenders: ContenderProps[];
+  forwardRef?: any;
+  transform?: string;
 }
 
 const Match: React.FunctionComponent<Props> = (props): JSX.Element => {
-  const { contenders } = props;
+  const { contenders, forwardRef, transform } = props;
+  const contendersBoundsRef: React.RefObject<Bounds[]> = React.useRef([]);
+  const margin = 50;
 
   return (
-    <g>
-      {contenders.map((contender, index) => (
-        <Contender
-          key={contender.name}
-          logo={contender.logo}
-          name={contender.name}
-          y={index * 150}
-        />
-      ))}
+    <g ref={forwardRef} transform={transform}>
+      {contenders.map((contender, index) => {
+        const [contenderBounds, contenderRef] = useBounds();
+        contendersBoundsRef.current![index] = contenderBounds;
+        // TODO: Move to utils
+        const sumPreviousHeight = contendersBoundsRef
+          .current!.slice(0, index)
+          .reduce(
+            (acc: number, bounds: Bounds) => acc + bounds.height + margin,
+            0,
+          );
+        return (
+          <Contender
+            key={contender.name}
+            forwardRef={contenderRef}
+            logo={contender.logo}
+            name={contender.name}
+            y={sumPreviousHeight}
+          />
+        );
+      })}
     </g>
   );
 };
