a943fa69200514a2e90ff2c4c115bf16b5c8f0db
Emmanuel Pilande <epilande@gmail.com>
2019-03-30 15:14:21 -0700

    :sparkles: Display Bracket in fight page

--- pages/fight.tsx
+++ pages/fight.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Link from "next/link";
 import styled from "styled-components";
+import { Bracket } from "../components/Bracket";
 import Layout from "../components/Layout";
 
 const Title = styled.h1`
@@ -8,10 +9,13 @@ const Title = styled.h1`
   font-family: ${(props) => props.theme.fonts.title};
 `;
 
+const matchup = [[["A", "B"], ["C", "D"]], [["A", "C"]], [["C"]]];
+
 const Fight: React.FunctionComponent = (): JSX.Element => {
   return (
     <Layout title="Fight | Battles.dev">
       <Title>Fight! 💥</Title>
+      <Bracket matchup={matchup} />
       <p>
         <Link href="/">
           <a>Back</a>
