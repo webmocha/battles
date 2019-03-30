a801cc5a238a7f9019f9cac2b6ff93713bad020b
Emmanuel Pilande <epilande@gmail.com>
2019-03-29 23:19:52 -0700

    :sparkles: Set up Bracket component

--- components/Bracket/Bracket.tsx
+++ components/Bracket/Bracket.tsx
@@ -1,7 +1,53 @@
 import * as React from "react";
+import useBounds from "../hooks/useBounds";
+import BracketStore, { BracketStoreContext } from "./Store";
+import Round from "./Round";
 
 const Bracket: React.FunctionComponent = (): JSX.Element => {
-  return <div>Hello World</div>;
+  const { state, dispatch } = React.useContext(BracketStoreContext);
+  const [bracketBounds, bracketRef] = useBounds();
+  const data = [
+    [
+      ["react", "vue"],
+      ["angular", "mithril"],
+      ["angular1", "mithril1"],
+      ["angular2", "mithril2"],
+    ],
+    [["react", "vue"], ["angular", "mithril"]],
+    [["vue", "angular"]],
+    [["angular"]],
+  ];
+
+  React.useLayoutEffect(() => {
+    dispatch({
+      type: "SET_HEIGHT",
+      height: bracketBounds.height > 0 ? bracketBounds.height : 250,
+    });
+  }, [bracketBounds.height]);
+
+  return (
+    <svg width={bracketBounds.width} height={state.height}>
+      <g ref={bracketRef}>
+        {data.map((matches, i) => (
+          <Round
+            height={state.height}
+            key={i}
+            x={i * 250}
+            round={i}
+            matches={matches}
+          />
+        ))}
+      </g>
+    </svg>
+  );
 };
 
-export default Bracket;
+const BracketConnected = (props: any): JSX.Element => {
+  return (
+    <BracketStore>
+      <Bracket {...props} />
+    </BracketStore>
+  );
+};
+
+export default BracketConnected;
