eef4a2ec63f870b2ad0dd4be84564bf67b7d3f8b
Emmanuel Pilande <epilande@gmail.com>
2019-04-01 13:44:36 -0700

    :truck: Update fonts
    Point to remote server
    Resolves  #27.

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,10 +1,10 @@
 const fonts = `
   @font-face {
     font-family: 'RoadRage';
-    src: url('/static/fonts/RoadRage.otf') format("opentype"),
-          url('/static/fonts/RoadRage.woff2') format('woff2'),
-          url('/static/fonts/RoadRage.woff') format('woff'),
-          url('/static/fonts/RoadRage.ttf') format('truetype');
+    src: url('https://static.battles.dev/fonts/RoadRage.otf') format("opentype"),
+          url('https://static.battles.dev/fonts/RoadRage.woff2') format('woff2'),
+          url('https://static.battles.dev/fonts/RoadRage.woff') format('woff'),
+          url('https://static.battles.dev/fonts/RoadRage.ttf') format('truetype');
     font-weight: normal;
     font-style: normal;
   }
