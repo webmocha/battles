dcd9c901c2429d2ec4330c3e073d181e67ccdd85
Emmanuel Pilande <epilande@gmail.com>
2019-04-01 13:33:05 -0700

    :bathtub: Clean up

--- components/Bracket/reducer.ts
+++ components/Bracket/reducer.ts
@@ -1,32 +1,22 @@
 interface State {
-  // width: number;
   height: number;
-  packages: {};
-  matches: [];
   highlight: string;
   animate: boolean;
 }
 
 export const initialState: State = {
-  // width: 0,
   height: 0,
-  packages: {},
-  matches: [],
   highlight: "",
   animate: false,
 };
 
 export type Action =
-  // | { type: "SET_WIDTH"; width: number }
   | { type: "SET_HEIGHT"; height: number }
   | { type: "SET_ANIMATE"; animate: boolean }
-  | { type: "SET_HIGHLIGHT"; name: string }
-  | { type: "LOAD_PACKAGES"; packages: {} };
+  | { type: "SET_HIGHLIGHT"; name: string };
 
 function bracketReducer(state: State, action: Action): State {
   switch (action.type) {
-    // case "SET_WIDTH":
-    //   return { ...state, width: action.width };
     case "SET_HEIGHT":
       return { ...state, height: action.height };
     case "SET_ANIMATE":
