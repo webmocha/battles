28acc636ade12c50792792da50882d89ae454733
Emmanuel Pilande <epilande@gmail.com>
2019-04-01 01:28:48 -0700

    :label: Add typings

--- api/fight.ts
+++ api/fight.ts
@@ -1,6 +1,19 @@
 import fetch from "isomorphic-unfetch";
 import { format, subDays } from "date-fns";
 
+export interface Download {
+  downloads: number;
+  day: string;
+}
+
+export interface DownloadsResponse {
+  [key: string]: {
+    downloads: Download[];
+    package: string;
+    outcome?: number;
+  };
+}
+
 export const getFightData = (packages: string): Promise<any> => {
   const twoDaysBefore = format(subDays(new Date(), 2), "YYYY-MM-DD");
   const oneDayBefore = format(subDays(new Date(), 1), "YYYY-MM-DD");
