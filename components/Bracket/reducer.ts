f0280963f94978ac40885ede102cc00506e67666
Emmanuel Pilande <epilande@gmail.com>
2019-04-03 08:27:26 -0700

    :card_file_box: Add details and packages handler to reducer

--- components/Bracket/reducer.ts
+++ components/Bracket/reducer.ts
@@ -1,8 +1,17 @@
+import { PackageEntry, DownloadsResponse } from "../../api/fight";
+
+interface Details extends PackageEntry {
+  start: string;
+  end: string;
+}
+
 interface State {
   height: number;
   highlight: string;
   animate: boolean;
   animationDelay: number;
+  details: Details[] | null;
+  packages: DownloadsResponse;
   // animationDuration: number;
 }
 
@@ -11,13 +20,17 @@ export const initialState: State = {
   highlight: "",
   animate: false,
   animationDelay: 2500,
+  details: null,
+  packages: {},
   // animationDuration: 0,
 };
 
 export type Action =
   | { type: "SET_HEIGHT"; height: number }
   | { type: "SET_ANIMATE"; animate: boolean }
-  | { type: "SET_HIGHLIGHT"; name: string };
+  | { type: "SET_HIGHLIGHT"; name: string }
+  | { type: "SET_DETAILS"; details: Details[] | null }
+  | { type: "SET_PACKAGES"; packages: DownloadsResponse };
 
 function bracketReducer(state: State, action: Action): State {
   switch (action.type) {
@@ -27,6 +40,10 @@ function bracketReducer(state: State, action: Action): State {
       return { ...state, animate: action.animate };
     case "SET_HIGHLIGHT":
       return { ...state, highlight: action.name };
+    case "SET_DETAILS":
+      return { ...state, details: action.details };
+    case "SET_PACKAGES":
+      return { ...state, packages: action.packages };
     default:
       throw new Error();
   }
