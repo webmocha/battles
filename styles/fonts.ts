315fd144f3b32de924a3ece1e174aaeb18103fdd
Emmanuel Pilande <epilande@gmail.com>
2019-04-05 14:32:04 -0700

    :package: Set static location based on NODE_ENV

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,10 +1,18 @@
 const fonts = `
   @font-face {
     font-family: 'RoadRage';
-    src: url('https://static.battles.dev/fonts/RoadRage.otf') format("opentype"),
-          url('https://static.battles.dev/fonts/RoadRage.woff2') format('woff2'),
-          url('https://static.battles.dev/fonts/RoadRage.woff') format('woff'),
-          url('https://static.battles.dev/fonts/RoadRage.ttf') format('truetype');
+    src: url('${
+      process.env.STATIC_LOCATION
+    }/fonts/RoadRage.otf') format("opentype"),
+          url('${
+            process.env.STATIC_LOCATION
+          }/fonts/RoadRage.woff2') format('woff2'),
+          url('${
+            process.env.STATIC_LOCATION
+          }/fonts/RoadRage.woff') format('woff'),
+          url('${
+            process.env.STATIC_LOCATION
+          }/fonts/RoadRage.ttf') format('truetype');
     font-weight: normal;
     font-style: normal;
   }
